6623020f9f7d66b723c0a45a0d700ee5112db8c6
Alex <lexa260011@ngs.ru>
2017-02-13 23:56:05 +0000

    begin HDD

---
+++ hardDrive/include/harddrive.h
@@ -0,0 +1,29 @@
+#ifndef BC_H
+#define BC_H
+
+typedef struct TCHS {
+	unsigned int countCylinder;
+	unsigned int countHead;
+	unsigned int countSector;
+} tCHS;
+
+typedef struct TLARGE { 
+	unsigned int countCylinder;
+	unsigned int countHead;
+	unsigned int countSector;
+} tLARGE;
+
+typedef struct TIDECHS {
+	unsigned int countCylinder;
+	unsigned int countHead;
+	unsigned int countSector;
+} tIDECHS;
+
+typedef struct TLBA { 
+	unsigned int countCylinder
+	unsigned int countHead;
+	unsigned int countSector;
+} tLBA;
+
+
+#endif
