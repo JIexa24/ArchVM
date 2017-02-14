9191878c75cfee21903a88f2898edebc1f919021
Alex <lexa260011@ngs.ru>
2017-02-14 08:13:17 +0000

    translation asm-command to int-code

---
+++ asm/include/asm.h
@@ -0,0 +1,13 @@
+#ifndef ASM_H
+#define ASM_H
+
+#include <stdio.h>
+#include <string.h>
+
+#include "./../../simpleComputer/include/sc_register.h"
+#include "./../../simpleComputer/include/sc_command.h"
+#include "./../../simpleComputer/include/sc_memory.h"
+
+int asmCommand(char *str);
+
+#endif
