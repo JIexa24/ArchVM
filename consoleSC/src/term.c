84f9f0e0e5917cbfa4ac6a0c07ac54cee448e9fe
Alex <lexa260011@ngs.ru>
2017-02-18 00:29:32 +0000

    optimize

--- consoleSC/src/term.c
+++ consoleSC/src/term.c
@@ -1,6 +1,20 @@
 #include "./../include/term.h"
 
-/*infocmp*/
+/*
+infocmp
+
+enum colors {
+	clr_black,
+	clr_red,
+	clr_green,
+	clr_brown,
+	clr_blue,
+	clr_magenta,
+	clr_cyan,
+	clr_light_blue,
+	clr_white,
+	clr_default
+};*/
 
 int mt_clrscr()
 {
@@ -35,72 +49,20 @@ int mt_getscreensize(int *rows, int *cols)
 /*------------------------------------------------------------------------------*/
 int mt_setfgcolor(enum colors color)
 {
-  switch (color) {
-    case clr_black:
-      printf("\E[30m");
-      break;
-    case clr_red:
-	  printf("\E[31m");
-      break;
-    case clr_green:
-      printf("\E[32m");
-      break;
-    case clr_brown:
-      printf("\E[33m");
-      break;	
-    case clr_blue:
-      printf("\E[34m");
-      break;
-    case clr_magenta:
-      printf("\E[35m");
-      break;
-    case clr_cyan:
-      printf("\E[36m");
-      break;
-    case clr_light_blue:
-      printf("\E[37m");
-      break;
-    case clr_default:
-      printf("\E[39m");
-      break;
-    default:
-      return -1;
+  if (color >= 0 && color <= 9) {
+    printf("\E[%dm", 30 + color);
+    return 0;
+  } else {
+    return -1;
   }
-  return 0;
 }
 /*------------------------------------------------------------------------------*/
 int mt_setbgcolor(enum colors color)
 {
-  switch (color) {
-    case clr_black:
-      printf("\E[40m");
-      break;
-    case clr_red:
-      printf("\E[41m");
-      break;
-    case clr_green:
-      printf("\E[42m");
-      break;
-    case clr_brown:
-      printf("\E[43m");
-      break;
-    case clr_blue:
-      printf("\E[44m");
-      break;
-    case clr_magenta:
-      printf("\E[45m");
-      break;
-    case clr_cyan:
-      printf("\E[46m");
-      break;
-    case clr_light_blue:
-      printf("\E[47m");
-      break;
-    case clr_default:
-      printf("\E[49m");
-      break;
-    default:
-      return -1;
+  if (color >= 0 && color <= 9) {
+    printf("\E[%dm", 40 + color);
+    return 0;
+  } else {
+    return -1;
   }
-  return 0;
 }
