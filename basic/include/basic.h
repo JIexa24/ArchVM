c671989bd473106f766255de758e463bdc8d2574
Alex <lexa260011@ngs.ru>
2017-02-22 16:45:31 +0000

    basic translate

--- basic/include/basic.h
+++ basic/include/basic.h
@@ -3,5 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum keywords {
+  KEYW_REM = 1,
+  KEYW_INPUT = 2,
+  KEYW_OUTPUT = 3,
+  KEYW_GOTO = 4,
+  KEYW_IF = 5,
+  KEYW_LET = 6,
+  KEYW_END = 7,
+  KEYW_E = -1
+};
+
+int keywordCode(char *str);
 
 #endif
--- basic/src/basic.c
+++ basic/src/basic.c
@@ -1,1 +1,23 @@
 #include "basic.h"
+
+
+int keywordCode(char *str)
+{
+  if (strcmp(str, "REM") == 0) {
+    return KEYW_REM;
+  } else if (strcmp(str, "INPUT") == 0) {
+    return KEYW_INPUT;
+  } else if (strcmp(str, "OUTPUT") == 0) {
+    return KEYW_OUTPUT;
+  } else if (strcmp(str, "GOTO") == 0) {
+    return KEYW_GOTO;
+  } else if (strcmp(str, "IF") == 0) {
+    return KEYW_IF;
+  } else if (strcmp(str, "LET") == 0) {
+    return KEYW_LET;
+  } else if ((strcmp(str, "END") == 0) || (strcmp(str, "END\n") == 0)) {
+    return KEYW_END;
+  } else {
+    return KEYW_E;
+  }
+}
